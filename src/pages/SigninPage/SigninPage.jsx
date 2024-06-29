fb56d0ac7c53322fc4c862206561cd7e304f53da
Lolita Yenik <lolitayenik@ukr.net>
2024-06-29 03:19:05 +0300

    added permissions

--- src/pages/SigninPage/SigninPage.jsx
+++ src/pages/SigninPage/SigninPage.jsx
@@ -1,7 +1,7 @@
 import Container from '../../components/common/Container/Container';
 import AuthForm from '../../components/forms/AuthForm/AuthForm';
 import Meta from '../../components/common/Meta/Meta';
-// import bottle from '../../assets/static/bottle-for-signin.svg';
+
 import s from './SigninPage.module.css';
 import AuthWrapper from '../../components/AuthWrapper/AuthWrapper';
 
